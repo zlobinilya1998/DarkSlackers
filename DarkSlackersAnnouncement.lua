b125bf4ab4a401282bf734e94f335eeb00dd50c9
zlobin_ilya <qwertin1998@gmail.com>
2023-08-08 03:06:38 +0300

    Add: loss control

--- DarkSlackersAnnouncement.lua
+++ DarkSlackersAnnouncement.lua
@@ -1,26 +1,44 @@
 function DS_Announcement_Onload()
+	this:RegisterEvent("PLAYER_ENTERING_WORLD");
 	this:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_SELF_BUFFS");
 	this:RegisterEvent("CHAT_MSG_SPELL_AURA_GONE_SELF");
+	this:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_SELF_DAMAGE");
 	this:RegisterEvent("CHAT_MSG_SPELL_SELF_DAMAGE");
 end
 
 
 
 function DS_Announcement_OnEvent()
-	target = UnitName("Target");
-	icon1, name1, active1, castable1 = GetShapeshiftFormInfo(1)
-	if (event == "CHAT_MSG_SPELL_PERIODIC_SELF_BUFFS") then DS_ON_CHAT_MSG_SPELL_PERIODIC_SELF_BUFFS();
+	if (event == "PLAYER_ENTERING_WORLD") then DS_INITIALIZE();
+	elseif (event == "CHAT_MSG_SPELL_PERIODIC_SELF_BUFFS") then DS_ON_CHAT_MSG_SPELL_PERIODIC_SELF_BUFFS();
     elseif (event == "CHAT_MSG_SPELL_AURA_GONE_SELF") then DS_ON_CHAT_MSG_SPELL_AURA_GONE_SELF();
     elseif (event == "CHAT_MSG_SPELL_SELF_DAMAGE") then DS_ON_CHAT_MSG_SPELL_SELF_DAMAGE();
+    elseif (event == "CHAT_MSG_SPELL_PERIODIC_SELF_DAMAGE") then DS_ON_CHAT_MSG_SPELL_PERIODIC_SELF_DAMAGE();
 	end
 end
 
-
-
+function DS_INITIALIZE()
+    if not DarkSlackersConfig then
+        DarkSlackersConfig = {
+            ["Loss of control"] = true,
+        }
+    end
+end
 
 function DS_Is_In_Raid()
     inInstance, instanceType = IsInInstance()
     return instanceType == 'raid'
+end
+
+function DS_Check_For_Buff(unit,name,app)
+    local i=1;
+    local state,apps;
+    while true do
+        state,apps = UnitBuff(unit,i);
+        if not state then return false end
+        if string.find(state,name) and ((app == apps) or (app == nil)) then return apps end
+        i=i+1;
+    end
 end
 
 function DS_Check_For_Debuff(unit,name,app)
@@ -34,8 +52,13 @@ function DS_Check_For_Debuff(unit,name,app)
     end
 end
 
-function DS_Announce_Spell(gainWhat)
-    if (gainWhat == nil or GetNumRaidMembers() == 0 or not DS_Is_In_Raid()) then return end
+function DS_Announce_Spell_Gain(gainWhat)
+--     if (gainWhat == nil or GetNumRaidMembers() == 0 or not DS_Is_In_Raid()) then return end
+
+--     hasCS = DS_Check_For_Buff('player',"Spell_Shadow_SoulGem");
+--     if (gainWhat == 'Soulstone Ressurection') then
+--         DEFAULT_CHAT_FRAME:AddMessage("SS gained")
+--     end
 end
 
 function DS_Announce_Spell_Gone(goneWhat)
@@ -43,9 +66,42 @@ function DS_Announce_Spell_Gone(goneWhat)
 end
 
 function DS_ON_CHAT_MSG_SPELL_PERIODIC_SELF_BUFFS()
-    local _, _, gainWhat = string.find(arg1, "You gain (.*).");
-    DS_Announce_Spell(gainWhat);
+    local _, _, gainWhat, time, r = string.find(arg1, "You gain (.*).");
+    DS_Announce_Spell_Gain(gainWhat);
 end
+
+function DS_ON_CHAT_MSG_SPELL_PERIODIC_SELF_DAMAGE()
+    DS_FILTER_DEBUFFS(arg1)
+end
+
+function DS_FILTER_DEBUFFS(spell)
+    if not DarkSlackersConfig["Loss of control"] then end
+
+    if string.find(spell, " Sap.") then
+        SendChatMessage("Sapped!","SAY")
+    elseif string.find(spell, " Freezing Trap.") then
+        SendChatMessage("Trapped!","SAY")
+    elseif string.find(spell, " Blind.") then
+        SendChatMessage("Blinded!","SAY")
+    elseif string.find(spell, "Polymorph") then
+        SendChatMessage("Polymorphed!","SAY")
+    elseif ( string.find(spell, "Fear") or string.find(spell, " Intimidating Shout.") or string.find(spell, " Psychic Scream.") ) then
+        SendChatMessage("Feared!","SAY")
+    elseif ( string.find(spell, " Hibernate.") or string.find(spell, " Sleep.") or string.find(spell, " Wyvern Sting.") ) then
+        SendChatMessage("Hibernated!","SAY")
+    elseif string.find(spell, " Reckless Charge.") then
+        SendChatMessage("Reckless Charged!","SAY")
+    elseif ( string.find(spell, "Silence") or string.find(spell, "Spell Lock.") ) then
+        SendChatMessage("Silenced!","SAY")
+    elseif string.find(spell, "Mind Control") then
+        SendChatMessage("Im on Mindcontroll!","SAY")
+    elseif string.find(spell, " Seduction.") then
+        SendChatMessage("Seduced!","SAY")
+    elseif (string.find(spell, " Cheap Shot.") or string.find(spell, " Kidney Shot.") ) then
+        SendChatMessage("Rogue on me!","SAY")
+    end
+end
+
 
 function DS_ON_CHAT_MSG_SPELL_AURA_GONE_SELF()
     local _, _, goneWhat = string.find(arg1, "(.*) fades from you.");
